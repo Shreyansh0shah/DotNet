2800d0d05500f515185cd757b6aadc11869f74b9
shahshreyansh <sshreyansh0000@gmail.com>
2025-12-01 17:01:13 +0530

    first commit

---
+++ PracticeApp/Radius.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeApp
+{
+    class Radius
+    {
+       static  void Main()
+        {
+            double r=3.14;
+            double radiu=3.14*r*r;
+            Console.WriteLine(radiu);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticeApp/Sum.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeApp
+{
+    // class Sum
+    // {
+    //    //static void Main(){
+    //     Console.WriteLine("hello");
+    
+    //     }
+    //}
+}
