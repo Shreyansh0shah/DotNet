f7420b9763c16d8d228d562fd36e57ceb740b30a
shahshreyansh <sshreyansh0000@gmail.com>
2025-12-01 17:25:05 +0530

    height

---
+++ PracticeApp/HightInCm.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeApp
+{
+    public class HightInCm
+    {
+        static void Main()
+        {
+            int h;
+            h=10;
+            float i;
+            i=2.14f;
+            
+            float height=i*h;
+            Console.WriteLine(height);
+            Console.ReadKey();
+
+
+        }
+    }
+}
